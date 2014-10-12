dad31060538be6738448b41cc2ed24407082859e
Andrey <iandrewka@gmail.com>
2014-10-12 17:13:38 +0900

    first change

---
+++ Example/src/Test1.java
@@ -0,0 +1,21 @@
+
+public class Test1 {
+
+	public static void main(String[] args){
+		
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+		
+	}
+	
+	private static void displayGreeting()
+	{
+		System.out.println("HELLO");
+		System.out.println("WORLD");
+		
+		System.out.println("Test1");
+	}
+}
+
